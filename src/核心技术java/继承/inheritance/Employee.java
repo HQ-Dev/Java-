735ab4653c259af052417f4baf17ecd2e43dba11
archy <hq_dev@163.com>
2016-05-27 02:47:46 +0800

    增加了员工/BOSS继承相关的代码

---
+++ src/核心技术java/继承/inheritance/Employee.java
@@ -0,0 +1,38 @@
+package src.核心技术java.继承.inheritance;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by archy on 5/27/16.
+ */
+public class Employee {
+    private String name;
+    private double salary;
+    private Date hireDay;
+
+    public Employee(String name, double salary, int year, int month, int day) {
+        this.name = name;
+        this.salary = salary;
+        GregorianCalendar calendar = new GregorianCalendar(year, month-1, day);
+        this.hireDay = calendar.getTime();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public Date getHireDay() {
+        return hireDay;
+    }
+
+    public void raiseSalary(double byPercent) {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+
+}
---
+++ src/核心技术java/继承/inheritance/Manager.java
@@ -0,0 +1,25 @@
+package src.核心技术java.继承.inheritance;
+
+/**
+ * Created by archy on 5/27/16.
+ */
+public class Manager extends Employee {
+    private double bonus;
+
+    public Manager(String name, double salary, int year, int month, int day) {
+        super(name, salary, year, month, day);
+        this.bonus = 0;
+    }
+
+
+    public double getSalary() {
+        double baseSalary = super.getSalary();
+        return baseSalary + bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+
+}
